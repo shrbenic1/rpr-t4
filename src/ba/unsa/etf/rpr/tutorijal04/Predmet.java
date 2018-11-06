d09b7edea1b70141a88de2ea6cd1a84f25f28cdb
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-06 17:50:49 +0100

    Dodani konstruktor i getteri za klasu Predmet

--- src/ba/unsa/etf/rpr/tutorijal04/Predmet.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Predmet.java
@@ -6,4 +6,21 @@ public class Predmet {
     private Integer ects;
     private Boolean izborni;
 
+    public Predmet(String ime, Integer ects, Boolean izborni) {
+        this.ime = ime;
+        this.ects = ects;
+        this.izborni = izborni;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public Integer getEcts() {
+        return ects;
+    }
+
+    public Boolean getIzborni() {
+        return izborni;
+    }
 }
