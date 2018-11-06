3a82cff85c67855a82c347b54ef5c60ad783f0fc
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-06 17:52:54 +0100

    Dodani konstruktor i getteri za klasu Fakultet

--- src/ba/unsa/etf/rpr/tutorijal04/Fakultet.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Fakultet.java
@@ -6,5 +6,23 @@ public class Fakultet {
     private Set<Student> spisakStudenata;
     private Set<Predmet> spisakPredmeta;
     private Set<Upis> spisakUpisa;
+
+    public Fakultet(Set<Student> spisakStudenata, Set<Predmet> spisakPredmeta, Set<Upis> spisakUpisa) {
+        this.spisakStudenata = spisakStudenata;
+        this.spisakPredmeta = spisakPredmeta;
+        this.spisakUpisa = spisakUpisa;
+    }
+
+    public Set<Student> getSpisakStudenata() {
+        return spisakStudenata;
+    }
+
+    public Set<Predmet> getSpisakPredmeta() {
+        return spisakPredmeta;
+    }
+
+    public Set<Upis> getSpisakUpisa() {
+        return spisakUpisa;
+    }
 }
 
