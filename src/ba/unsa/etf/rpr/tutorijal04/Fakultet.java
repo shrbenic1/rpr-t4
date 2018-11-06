f10208147419ca8a8a6e1cafd11b7ea57a43efad
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-06 17:43:31 +0100

    Atributi Fakulteta promijenjeni iz List u Set

--- src/ba/unsa/etf/rpr/tutorijal04/Fakultet.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Fakultet.java
@@ -1,10 +1,10 @@
 package ba.unsa.etf.rpr.tutorijal04;
 
-import java.util.List;
+import java.util.Set;
 
 public class Fakultet {
-    private List<Student> spisakStudenata;
-    private List<Predmet> spisakPredmeta;
-    private List<Upis> spisakUpisa;
+    private Set<Student> spisakStudenata;
+    private Set<Predmet> spisakPredmeta;
+    private Set<Upis> spisakUpisa;
 }
 
