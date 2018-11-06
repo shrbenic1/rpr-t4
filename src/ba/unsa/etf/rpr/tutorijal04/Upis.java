8e5d453d86857b51f03c63affdb0417dd73491e9
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-06 17:52:23 +0100

    Dodani konstruktor i getteri za klasu Upis

--- src/ba/unsa/etf/rpr/tutorijal04/Upis.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Upis.java
@@ -5,4 +5,21 @@ public class Upis {
     private Predmet predmet;
     private Student student;
 
+    public Upis(PlanStudija planStudija, Predmet predmet, Student student) {
+        this.planStudija = planStudija;
+        this.predmet = predmet;
+        this.student = student;
+    }
+
+    public PlanStudija getPlanStudija() {
+        return planStudija;
+    }
+
+    public Predmet getPredmet() {
+        return predmet;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
 }
