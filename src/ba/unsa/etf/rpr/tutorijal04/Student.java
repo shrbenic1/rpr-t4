68c71ea6bc0c7406d03cabf681b297ef4e065775
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-06 17:50:22 +0100

    Dodani konstruktor i getteri za klasu Student

--- src/ba/unsa/etf/rpr/tutorijal04/Student.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Student.java
@@ -1,10 +1,26 @@
 package ba.unsa.etf.rpr.tutorijal04;
 
-import java.util.ArrayList;
 
 public class Student {
     private String ime;
     private String prezime;
     private Integer brojIndeksa;
 
+    public Student(String ime, String prezime, Integer brojIndeksa) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.brojIndeksa = brojIndeksa;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public Integer getBrojIndeksa() {
+        return brojIndeksa;
+    }
 }
