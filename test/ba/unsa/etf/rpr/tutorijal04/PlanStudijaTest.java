90303b1171445c510ad8200b27d0b1c3a49f28cf
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-10 23:10:27 +0100

    Kreiran test za klasu PlanStudija

---
+++ test/ba/unsa/etf/rpr/tutorijal04/PlanStudijaTest.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rpr.tutorijal04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlanStudijaTest {
+    @Test
+    void planStudijaTest() {
+        Student student = new Student("Sven", "Hrbenić", 17901);
+        PlanStudija planStudija = new PlanStudija();
+        Predmet razvojProgramskihRjesenja = new Predmet("RPR", 5, true);
+        Predmet digitalniSistemiUpravljanja = new Predmet("DSU", 5, false);
+        Predmet osnoveRacunarstva = new Predmet("OR", 6, false);
+        Predmet inzenjerskaMatematika1 = new Predmet("IM1", 7, false);
+        planStudija.dodajPredmet(3, razvojProgramskihRjesenja);
+        planStudija.dodajPredmet(5, digitalniSistemiUpravljanja);
+        planStudija.dodajPredmet(1, osnoveRacunarstva);
+        planStudija.dodajPredmet(1, inzenjerskaMatematika1);
+        Upis upis = new Upis(planStudija, razvojProgramskihRjesenja , student);
+        assertEquals(planStudija, upis.getPlanStudija());
+    }
+
+}
